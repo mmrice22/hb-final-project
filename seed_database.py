85f00c358011bcbb85b32fc851f701089fb9b621
Melissa Rice <mmrice22@gmail.com>
2021-04-07 15:48:51 +0000

    using seed.py to add 5 users to the user table

--- seed_database.py
+++ seed_database.py
@@ -4,13 +4,38 @@ import os
 import json
 from random import choice, randint
 
-import model
+from model import db, User, NationalPark, Favorite, ParkActivity, Activity, connect_to_db
 import crud
 import server
 
 os.system('dropdb parks')
 os.system('createdb parks')
 
-model.connect_to_db(server.app)
-model.db.create_all()
+connect_to_db(server.app)
+db.create_all()
     
+# Make 5 Users to populate tables
+user1 = User(fname = 'Mel', lname = 'Rice', 
+            email = 'ricemel1@msu.edu', password = 'test1')
+db.session.add(user1)
+db.session.commit()
+
+user2 = User(fname = 'Jess', lname = 'Rice', 
+            email = 'ricejes1@msu.edu', password = 'test2')
+db.session.add(user2)
+db.session.commit()
+
+user3 = User(fname = 'Bailey', lname = 'Rice', 
+            email = 'ricebail1@msu.edu', password = 'test3')
+db.session.add(user3)
+db.session.commit()
+
+user4 = User(fname = 'Glenda', lname = 'Rice', 
+            email = 'gfrice@icloud.com', password = 'test4')
+db.session.add(user4)
+db.session.commit()
+
+user5 = User(fname = 'Walter', lname = 'Rice', 
+            email = 'wjrice@um.edu', password = 'test5')
+db.session.add(user5)
+db.session.commit()
