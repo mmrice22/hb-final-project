a3956e884cf1040f7025d1d53ecbcd130e2726e9
Melissa Rice <mmrice22@gmail.com>
2021-04-22 20:16:03 +0000

    added divData variable on click event function

--- static/script.js
+++ static/script.js
@@ -9,8 +9,26 @@ const disableFaveButton = () => {
 
 $('.favorite-button').on('click', (evt) => {
     $(evt.target).addClass("clicked");
+    // get info from the div that this button was inside of
+    const divData = {
+        // get info stored in element with the class park-name
+        parkName: $('.park-name').data(),
+        // get info stored in element with the class park-description
+        parkDescription: $('.park-description').data(),
+        // get info stored in element with the class park-directions
+        parkDirections: $('.park-directions').data(),
+        // get info stored in element with the class state-code
+        parkStateCode: $('.state-code').data()
+    };
     disableFaveButton(evt.target);
 });
+
+
+// send divData to the server
+$.get('/favorites', divData, (res) => {
+    //display response from the server
+    
+})
 
 
 
