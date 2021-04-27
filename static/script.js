b9c8c547d32c7db8e887e959d6ec4588221f80ea
Melissa Rice <mmrice22@gmail.com>
2021-04-27 19:36:49 +0000

    checking for right response

--- static/script.js
+++ static/script.js
@@ -9,13 +9,14 @@ const disableFaveButton = () => {
 
 $('.favorite-button').on('click', (evt) => {
     $(evt.target).addClass("clicked");
-    console.log(evt.target.value)
+    //console.log(evt.target.value)
     // get info from the div that this button was inside of
     const parkCode = evt.target.value
     // get park data from the route it is in whish is the parks/search
     $.get('/makefavorite', {"parkCode": parkCode}, (res) => {
     //display response from the server
         alert(`This is the response -> ${res}`);
+        
 });
     disableFaveButton(evt.target);
 });
