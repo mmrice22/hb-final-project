b91ab3457178864e672e8242d03dab526f28738f
Melissa Rice <mmrice22@gmail.com>
2021-04-28 02:31:45 +0000

    check button disables on click

--- static/script.js
+++ static/script.js
@@ -13,12 +13,34 @@ $('.favorite-button').on('click', (evt) => {
     // get info from the div that this button was inside of
     const parkCode = evt.target.value
     // get park data from the route it is in whish is the parks/search
-    $.get('/makefavorite', {"parkCode": parkCode}, (res) => {
+    $.get('/makefavorite.json', {"parkCode": parkCode}, (res) => {
     //display response from the server
         alert(`You've successfully added ${res} to your favorites!`);
         
 });
     disableFaveButton(evt.target);
+});
+
+
+
+const disableCheckButton = () => {
+    if ($("button").hasClass("clicked")){
+        $("button.clicked").attr('disabled', true)
+    };  
+};
+
+
+$('.been-button').on('click', (evt) => {
+    $(evt.target).addClass("clicked");
+    console.log(evt.target);
+    const data = {
+        'hasBeen': $('.has-been').val()
+    };
+
+    $.post('/visited.json', data, (res) => {
+        alert(res)
+    });
+    disableCheckButton(evt.target);
 });
 
 
